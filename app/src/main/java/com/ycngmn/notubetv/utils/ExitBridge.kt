28d9e62c4d7ac48b88b58aa59dda8566c1f00793
Manas <saparbektv@gmail.com>
2026-09-12 14:38:06 +0500

    Modified by www.SourceFiles.app

--- app/src/main/java/com/ycngmn/notubetv/utils/ExitBridge.kt
+++
@@ -1,9 +0,0 @@
-package com.ycngmn.notubetv.utils
-
-import android.webkit.JavascriptInterface
-import androidx.compose.runtime.MutableState
-
-class ExitBridge(val exitTrigger: MutableState<Boolean>) {
-    @JavascriptInterface
-    fun onExitCalled() { exitTrigger.value = true }
-}
